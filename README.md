2e0b36f54a8a53cab0fbd6dbf88f8fcf663ff29e
Kid Lei <eric@collectiveequity.com>
2024-02-13 15:15:43 +0000

    Wrote the whole program, tested it and added a Readme

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
-# team-profile-generator
-A generator which creates a team profile!
+# Eric's Team Profile Generator
+  [![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
+
+  ## Description
+  This is a node.js application that allows the user to generate a HTML team profile file from the console built with the Inquirer package, test driven development and multiple classes / subclasses.
+
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+
+  ## Installation
+  Node.js and the Inquirer (v.6.5.2, the latest version of Inquirer will not work) package from NPM are required to use this application.
+
+  The Jest package from NPM is required to test this application.
+
+  Follow the instructions at [Node.js](https://nodejs.org/en) to download and install Node.js.
+
+  Once Node.js is installed, you can install the Inquirer and Jest packages by first initialising NPM in the local folder, you can do this via the "npm init -y" command on console, and then installing Inquirer v.6.5.2 by using the "npm i inquirer@6.5.2" command.
+
+  Run "npm i jest" to install the Jest package.
+
+  ## Usage
+  Please open the console, and use the command "node index.js" and follow the prompts.
+
+  Your generated team profile HTML file will be in the output folder. Please use a browser to view your generated team profile. Please note - running a new instance of team profile generator will override the old team profile file in the output folder. To avoid deleting previous team profiles, move those generated files out of the output folder before generating a new team profile.
+
+  The generated team profile should look like this:
+  ![Example Team Profile](./assets/SampleHTML.png)
+
+  ## License
+  This application is released under the GNU General Public License v3.0 license. Please refer to the license file for more information.
+
+  ## Contributing
+  Feel free to take this code and build something else. This is a relatively simple project so I will not be updating it!
+
+  ## Tests
+  To run a rest, simply run "npm run test" via the console for Jest testing.
+
+  Otherwise, feel free to create a simple team profile with one manager, one engineer and one intern by following the prompts.
+
+  ## Questions
+  For any questions, please contact the developer via either of the following:
+  - [Github](https://github.com/KidLeiS)
+  - Email: [eric.shen123@gmail.com](eric.shen123@gmail.com)
+  
--- index.js
+++ index.js
@@ -10,6 +10,79 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
 
+// Empty team array
+const team = [];
 
-// TODO: Write Code to gather information about the development team members, and render the HTML file.
+// Questions for the Manager prompts
+const initQs = [
+    { name: "Name", type: "input", message: "What is the team manager's name? "},
+    { name: "ID", type: "input", message: "What is the team manager's employee ID? "},
+    { name: "Email", type: "input", message: "What is the team manager's email? "},
+    { name: "Office", type: "input", message: "What is the team manager's office number? "}
+];
 
+// Questions for the Engineer prompts
+const engineerQs = [
+    { name: "Name", type: "input", message: "What is the engineer's name? "},
+    { name: "ID", type: "input", message: "What is the engineer's employee ID? "},
+    { name: "Email", type: "input", message: "What is the engineer's email? "},
+    { name: "Github", type: "input", message: "What is the engineer's Github username? "}
+];
+
+// Questions for the Intern prompts
+const internQs = [
+    { name: "Name", type: "input", message: "What is the inten's name? "},
+    { name: "ID", type: "input", message: "What is the intern's employee ID? "},
+    { name: "Email", type: "input", message: "What is the intern's email? "},
+    { name: "School", type: "input", message: "What school does the intern go to? "}
+];
+
+// Code to enter a new engineer (and returning user to the team creation screen)
+const newEngineer = () => {
+    inquirer.prompt(engineerQs).then((ans) => {
+        team.push(new Engineer(ans.Name, ans.ID, ans.Email, ans.Github));
+        teamCreation();
+    })
+}
+
+// Code to enter a new intern (and returning user to the team creation screen)
+const newIntern = () => {
+    inquirer.prompt(internQs).then((ans) => {
+        team.push(new Intern(ans.Name, ans.ID, ans.Email, ans.School));
+        teamCreation();
+    })
+}
+
+// Team creation screen - users have 3 options each instance
+const teamCreation = () => {
+    inquirer.prompt([
+        { name: "selector", type: "list", message: "Choose an option:", choices: ["Add an engineer", "Add an intern", "Finish building the team"]}
+    ]).then((ans) => {
+        if (ans.selector === "Add an engineer") {
+            newEngineer();
+        } else if (ans.selector === "Add an intern") {
+            newIntern();
+        } else if (ans.selector === "Finish building the team") {
+
+            // To render the HTML file and stop returning the user back to the team creation screen
+            fs.writeFile(outputPath, render(team), (err) => {
+                err ? console.error('Error writing file'): console.log('Team profile generated successfully!');
+            });
+        }
+    })
+}
+
+// Init function, which creates the Manager entry and then directs user to team creation screen
+const init = () => {
+    inquirer.prompt(initQs).then((ans) => {
+        team.push(new Manager(ans.Name, ans.ID, ans.Email, ans.Office));
+
+        teamCreation();
+    })
+
+    
+}
+
+
+// To start the function
+init();
